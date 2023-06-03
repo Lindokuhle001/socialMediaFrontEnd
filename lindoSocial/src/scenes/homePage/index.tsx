912fd0b6ff4c029522ac34006fac7edf4aa3a837
lindokuhle shabalala <mlixshaba@gmail.com>
2023-06-03 17:34:44 +0200

    reated navbar

--- lindoSocial/src/scenes/homePage/index.tsx
+++ lindoSocial/src/scenes/homePage/index.tsx
@@ -1,9 +1,11 @@
 import { Box } from "@mui/material";
 import { ReactElement } from "react";
+import Navbar from "../navbar";
 
 const HomePage = (): ReactElement => {
   return (
     <Box>
+      <Navbar/>
       <div>HomePage</div>
     </Box>
   );
